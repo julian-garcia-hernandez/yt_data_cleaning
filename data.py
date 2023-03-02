77c6086227f724ed304362a628bc3cf6653b7c43
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2023-03-02 09:19:38 -0600

    added link to dataset

--- data.py
+++ data.py
@@ -16,3 +16,9 @@ sns.set() #sets the theme of graphs
 pd.set_option('display.max_columns', None) #set the value of max columns to unlimited
 pd.set_option('display.max_rows', None) #set the value of max rows to unlimited
 pd.options.mode.chained_assignment = None #
+
+AllCSV = [i for i in glob.glob('*.{}'.format('.csv'))]
+AllCSV
+
+#link to dataset
+#https://www.kaggle.com/datasets/datasnaek/youtube-new?resource=download&select=CA_category_id.json
