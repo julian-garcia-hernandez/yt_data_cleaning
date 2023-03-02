bd45dd50a5fc819b682ec9e30c49b50d0035e0ef
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2023-03-02 09:12:05 -0600

    some imports and configurations

--- data.py
+++ data.py
@@ -1,1 +1,18 @@
-print("initial commit")
+import numpy as np
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+from matplotlib import cm
+from datetime import datetime
+import glob
+import os
+import json
+import pickle
+import six
+
+sns.set() #sets the theme of graphs
+pd.set_option('display.max_columns', None) #set the value of max columns to unlimited
+pd.set_option('display.max_rows', None) #set the value of max rows to unlimited
+pd.options.mode.chained_assignment = None #
